8c7f8bbcb02f882f01fa8ba12ac11e2866535632
Sultanomar0013 <sultanomar756@gmail.com>
2024-01-07 21:34:01 +0600

    containe most water

--- 11.js
+++ 11.js
@@ -6,3 +6,25 @@ var maxArea = function(H) {
     }
     return ans
 };
+
+
+
+
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let result = [];
+
+    while(left < right) {
+        if(height[left] <= height[right]) {
+            let diff = Math.min(height[left], height[right]) * (right-left)
+            result.push(diff)
+            left ++;
+        } else {
+            let diff = Math.min(height[left], height[right]) * (right-left)
+            result.push(diff)
+            right--;
+        }
+    }
+    return Math.max(...result)
+};
